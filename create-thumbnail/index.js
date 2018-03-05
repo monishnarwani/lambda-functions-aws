b855f01c39c6ea40733de1007d53dd06432e1378
Monish Narwnai <monish.narwani@foclaworks.in>
2018-03-05 19:06:07 +0530

    laptime added

--- create-thumbnail/index.js
+++ create-thumbnail/index.js
@@ -11,23 +11,36 @@ exports.handler = (event, context, callback) => {
   console.log(srcBucket, destBucket, event)
   let imgName = event.Records[0].s3.object.key;
 
+  let watermarkImg = './copy1.png';
+
   let width = 200;
   let height = 300;
+  let offset = 75;
+  let orientation = 'portrait';
 
   readFileFromBucket(srcBucket, imgName).then(response => {
     let img = response.Body
 
-    createThumbnail(img, width, height).then(thumbnail => {
-      writeFileToBucket(destBucket, 'thumbnail_' + imgName, thumbnail, 'image/jpeg').then(response => {
-        callback(null, JSON.stringify(response))
+    gm(img).size((err, value) => {
+      if (value && (value.width > value.height)) {
+        width = 300;
+        height = 200;
+        offset = 0;
+        orientation = 'landscape';
+      }
+      console.log(width, height, offset, orientation)
+      createThumbnail(img, width, height, offset, orientation, watermarkImg).then(thumbnail => {
+        writeFileToBucket(destBucket, 'thumbnail_' + imgName, thumbnail, 'image/jpeg').then(response => {
+          callback(null, JSON.stringify(response))
+        }).catch(err => {
+          console.log(err.message, 'error in writing to dest')
+          callback('error in writing to dest')
+        })
       }).catch(err => {
-        console.log(err.message, 'error in writing to dest')
-        callback('error in writing to dest')
-      })
-    }).catch(err => {
-      console.log(err.message, 'error in creating thumbnail img')
-      callback('error in creating thumbnail img')
-    });
+        console.log(err.message, 'error in creating thumbnail img')
+        callback('error in creating thumbnail img')
+      });
+    })
 
   }).catch(err => {
     console.log(err, err.message, err.code) // message: 'The specified key does not exist.',code: 'NoSuchKey'
@@ -71,12 +84,21 @@ function writeFileToBucket (bucketName, fileName, content, contentType) {
   })
 }
 
-function createThumbnail(img, width, height) {
+function createThumbnail(img, width, height, offset, orientation, watermarkImg) {
+
+  console.log(watermarkImg)
+
 
   return new Promise((resolve, reject) => {
     console.log('creating thumbnail image/jpeg');
+    let geometry = width + 'x' + height + '+0+' + offset
+    console.log(geometry)
     gm(img).resize(width, height, '^')
       .gravity('center')
+      .composite(watermarkImg)
+      .geometry(geometry)
+      .font("Helvetica.ttf", 12)
+      .drawText(30, 20, "GMagick!")
       .toBuffer('jpg', (err, imgBuffer) => {
         if (err) {
           return reject(err)
